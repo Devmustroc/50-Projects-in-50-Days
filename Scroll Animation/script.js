06b1513dfc632f56dcac2a61129c350f9bbd5411
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-03-05 18:09:41 +0100

    Scroll animation

--- Scroll Animation/script.js
+++ Scroll Animation/script.js
@@ -1,7 +1,18 @@
-const boxes = document.querySelectorAll('.box');
+const boxes = document.querySelectorAll('.box');    // select all the elements with the class 'box'
 
-window.addEventListener('scroll', checkBoxes);
+window.addEventListener('scroll', checkBoxes); // when the user scrolls, run the function checkBoxes
 
+checkBoxes(); // run the function checkBoxes
 function checkBoxes() {
-    window.innerHeight / 5 * 4;
+    const triggerBottom = window.innerHeight / 5 * 4; // the height of the viewport divided by 5 and multiplied by 4
+
+    boxes.forEach(box => {
+        const boxTop = box.getBoundingClientRect().top // getBoundingClientRect() returns the size of an element and its position relative to the viewport.
+
+        if (boxTop < triggerBottom) { // if the box is in the viewport
+            box.classList.add('show') // add the class 'show' to the box
+        } else { // if the box is not in the viewport
+            box.classList.remove('show') // remove the class 'show' from the box
+        }
+    });
 }
